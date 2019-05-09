33e510e881b0cc81a3cd5aea24b11b07b231b79c
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-05-09 16:16:47 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
 def reformat_languages(languages)
   # your code here
+  new_hash ={}
+  languages.each do |hash, data|
+    data.each do |key, pair|
+      if key == :oo || key == :functional
+      binding.pry
+      
+    end
+  end
 end
+end
+
